fad4ec7008b272392c70abaf8246b92437d23bfb
tmdrl5779 <tmdrl5779@gmail.com>
2021-12-21 18:26:02 +0900

    [ADD] JwtAuthenticationEntryPoint - 유효한 자격증명이 없으면 401에러

---
+++ src/main/java/com/example/reservation/jwt/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,23 @@
+package com.example.reservation.jwt;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    /**
+     * 유효한 자격증명이 아니면 401에러
+     */
+    
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+    }
+}
